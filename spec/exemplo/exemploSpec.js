a5d976d4e585acc0a8d0b489eb62a261892a2678
ThiagoMarques <thiagossmarques@gmail.com>
2021-09-06 15:53:14 -0300

    Atualização de arquivos iniciais

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,8 @@
+describe("Suite de teste de exemplo com NodeJS", function(){
+    it("Deve demonstrar o uso do jasmine com o NodeJS", function(){
+        expect(true).toEqual(true);
+    });
+    it("Deve demonstrar o uso do karma em modo de escuta", function(){
+        expect(1).toEqual(1);
+    });
+});
